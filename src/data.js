2c34bc1260369d6252de3304d667599a68c30be8
Nathan <68994507+nathan-uch@users.noreply.github.com>
2022-08-30 16:05:02 -0700

    Fixed create row and added problem info and row.

--- src/data.js
+++ src/data.js
@@ -1,7 +1,9 @@
 import { BoxBlurInfo }  from '../src/solutions/box_blur.js';
 import { Minesweeper } from './solutions/minesweeper.js';
+import { CenturyFromYear } from './solutions/century_from_year';
 
 export const AllAlgos = [
     BoxBlurInfo,
-    Minesweeper
+    Minesweeper,
+    CenturyFromYear
 ]
---
+++ src/solutions/century_from_year.js
@@ -0,0 +1,30 @@
+import ProblemInfo from '../info_template.js';
+
+export const CenturyFromYear = new ProblemInfo;
+CenturyFromYear.name = 'Century from Year';
+CenturyFromYear.website = 'Code Signal';
+CenturyFromYear.link = 'https://app.codesignal.com/arcade/intro/level-1/egbueTZRRL5Mm4TXN/solutions';
+CenturyFromYear.date = 'Aug 30, 2022';
+CenturyFromYear.difficulty = 'Easy';
+CenturyFromYear.recommendedTime = 15;
+CenturyFromYear.timeToComplete = 0;
+CenturyFromYear.complete = true;
+CenturyFromYear.problem = 'Convert year to century';
+
+/* Problem & Example:
+
+Example:
+
+Equals:
+
+
+Steps:
+
+
+-------------------------- Pseudo Code ------------------------
+
+*/
+
+function solution(year) {
+    
+}
