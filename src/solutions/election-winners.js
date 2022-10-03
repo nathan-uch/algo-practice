b8dc6670abdeea613aaca883ad21ca2fb61a6824
Nathan <68994507+nathan-uch@users.noreply.github.com>
2022-10-02 22:25:15 -0700

    Tried elections winners

---
+++ src/solutions/election-winners.js
@@ -0,0 +1,26 @@
+import ProblemInfo from '../info-template.js';
+
+export const ElectionsWinnders = new ProblemInfo;
+ElectionsWinnders.name = 'Elections Winners';
+ElectionsWinnders.website = 'Code Signal';
+ElectionsWinnders.link = 'https://app.codesignal.com/arcade/intro/level-10/8RiRRM3yvbuAd3MNg';
+ElectionsWinnders.date = 'Oct 2st, 2022';
+ElectionsWinnders.difficulty = 'Medium';
+ElectionsWinnders.recommendedTime = 15;
+ElectionsWinnders.timeToComplete = 0;
+ElectionsWinnders.complete = true;
+ElectionsWinnders.problem = '';
+ElectionsWinnders.topics = '';
+ElectionsWinnders.usedHelp = false;
+ElectionsWinnders.pseudoCode = `
+Steps:
+
+`;
+
+/* Problem & Example:
+
+*/
+
+function solution(votes, k) {
+
+}
