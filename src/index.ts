bb189054db32137a37b8fba9618ea643bc38940d
Nathan <68994507+nathan-uch@users.noreply.github.com>
2022-10-11 11:54:43 -0700

    Added index file

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import CreateRow from "./src/components/create-row.js";
-import { AllAlgos } from "components/data.js";
+import CreateRow from "./components/create-row.js";
+import { AllAlgos } from "./components/data.js";
 
-(function PopulateTables() {
+(function PopulateTables(): void {
     let e = 0;
     let m = 0;
     let h = 0;
@@ -10,36 +10,36 @@ import { AllAlgos } from "components/data.js";
     let hnot = 0;
     for (let k = 0; k < AllAlgos.length; k++) {
         CreateRow(AllAlgos[k]);
-        if (AllAlgos[k].difficulty.toLowerCase() === 'easy' && AllAlgos[k].complete === true) {
+        if (AllAlgos[k].difficulty.toLowerCase() === 'easy' && AllAlgos[k].completed === true) {
             e++;
-        } else if (AllAlgos[k].difficulty.toLowerCase() === 'medium' && AllAlgos[k].complete === true) {
+        } else if (AllAlgos[k].difficulty.toLowerCase() === 'medium' && AllAlgos[k].completed === true) {
             m++;
-        } else if (AllAlgos[k].difficulty.toLowerCase() === 'hard' && AllAlgos[k].complete === true) {
+        } else if (AllAlgos[k].difficulty.toLowerCase() === 'hard' && AllAlgos[k].completed === true) {
             h++;
-        } else if (AllAlgos[k].difficulty.toLowerCase() === 'easy' && AllAlgos[k].complete === false) {
+        } else if (AllAlgos[k].difficulty.toLowerCase() === 'easy' && AllAlgos[k].completed === false) {
             mnot++;
-        } else if (AllAlgos[k].difficulty.toLowerCase() === 'medium' && AllAlgos[k].complete === false) {
+        } else if (AllAlgos[k].difficulty.toLowerCase() === 'medium' && AllAlgos[k].completed === false) {
             mnot++;
-        } else if (AllAlgos[k].difficulty.toLowerCase() === 'hard' && AllAlgos[k].complete === false) {
+        } else if (AllAlgos[k].difficulty.toLowerCase() === 'hard' && AllAlgos[k].completed === false) {
             hnot++;
         }
     }
 
-    const $easyCompleted = document.getElementById('easy-completed');
-    $easyCompleted.textContent = e;
-    const $mediumCompleted = document.getElementById('medium-completed');
-    $mediumCompleted.textContent = m;
-    const $hardCompleted = document.getElementById('hard-completed');
-    $hardCompleted.textContent = h;
-    const $questionsCompleted = document.getElementById('total-completed');
-    $questionsCompleted.textContent = Number(e) + Number(m) + Number(h);
+    const $easyCompleted = document.getElementById('easy-completed')!;
+    $easyCompleted.textContent = e.toString();
+    const $mediumCompleted = document.getElementById('medium-completed')!;
+    $mediumCompleted.textContent = m.toString();
+    const $hardCompleted = document.getElementById('hard-completed')!;
+    $hardCompleted.textContent = h.toString();
+    const $questionsCompleted = document.getElementById('total-completed')!;
+    $questionsCompleted.textContent = (Number(e) + Number(m) + Number(h)).toString();
 
-    const $easyNotCompleted = document.getElementById('easy-not-completed');
-    $easyNotCompleted.textContent = enot;
-    const $mediumNotCompleted = document.getElementById('medium-not-completed');
-    $mediumNotCompleted.textContent = mnot;
-    const $hardNotCompleted = document.getElementById('hard-not-completed');
-    $hardNotCompleted.textContent = hnot;
-    const $totalNotCompleted = document.getElementById('total-not-completed');
-    $totalNotCompleted.textContent = Number(enot) + Number(mnot) + Number(hnot);
+    const $easyNotCompleted = document.getElementById('easy-not-completed')!;
+    $easyNotCompleted.textContent = enot.toString();
+    const $mediumNotCompleted = document.getElementById('medium-not-completed')!;
+    $mediumNotCompleted.textContent = mnot.toString();
+    const $hardNotCompleted = document.getElementById('hard-not-completed')!;
+    $hardNotCompleted.textContent = hnot.toString();
+    const $totalNotCompleted = document.getElementById('total-not-completed')!;
+    $totalNotCompleted.textContent = (Number(enot) + Number(mnot) + Number(hnot)).toString();
 } ())
