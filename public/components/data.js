9b016e1b6f8afcae443ed65169a8d83055cb24b8
Nathan <68994507+nathan-uch@users.noreply.github.com>
2022-10-24 14:25:43 -0700

    Completed Chess Knight.

--- public/components/data.js
+++ public/components/data.js
@@ -39,7 +39,7 @@ import { IsMAC48Address } from '../solutions/is-mac-48-address.js';
 import { TwoSum } from '../solutions/two-sum.js';
 import { IsDigit } from '../solutions/is-digit.js';
 import { LineEncoding } from '../solutions/line-encoding.js';
-import { NumberOfIslands } from '../solutions/number-of-islands.js';
+import { ChessKnight } from '../solutions/chess-knight.js';
 export const AllAlgos = [
     BoxBlurInfo,
     Minesweeper,
@@ -82,5 +82,5 @@ export const AllAlgos = [
     TwoSum,
     IsDigit,
     LineEncoding,
-    NumberOfIslands,
+    ChessKnight,
 ];
--- src/components/data.ts
+++ src/components/data.ts
@@ -40,7 +40,7 @@ import { IsMAC48Address } from '../solutions/is-mac-48-address.js';
 import { TwoSum } from '../solutions/two-sum.js';
 import { IsDigit } from '../solutions/is-digit.js';
 import { LineEncoding } from '../solutions/line-encoding.js';
-import { NumberOfIslands } from '../solutions/number-of-islands.js';
+import { ChessKnight } from '../solutions/chess-knight.js';
 
 export const AllAlgos: ProblemInfo[] = [
   BoxBlurInfo,
@@ -84,5 +84,5 @@ export const AllAlgos: ProblemInfo[] = [
   TwoSum,
   IsDigit,
   LineEncoding,
-  NumberOfIslands,
+  ChessKnight,
 ];
